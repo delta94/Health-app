d90701754193bb532097b9948e34f35795ead0a1
Jun Nguyen <jun.nguyen@ecoxplore.dev>
2023-10-14 01:46:40 +0700

    feat: create Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,108 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import { Chart, registerables } from 'chart.js';
+
+import LineChart from '@/components/atoms/LineChart';
+import { FilterList } from '@/components/molecules/FilterList';
+import { MealList } from '@/components/organisms/MealList';
+import ProgressSection from '@/components/organisms/ProgressSection';
+import { useGetMeals } from '@/services';
+import { MealTime } from '@/types';
+import { Box, Button, Center, Container, SimpleGrid, Skeleton } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useMeasure } from 'react-use';
+
+Chart.register(...registerables);
+
+const data = {
+  labels: ['6月', '7月', '8月', '9月', '10月', '11月', '12月', '1月', '2月', '3月', '4月', '5月'],
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [12, 19, 3, 5, 2, 3, 7, 5, 6, 7, 8, 9],
+      borderColor: 'cyan',
+      backgroundColor: 'transparent',
+      pointBackgroundColor: 'cyan',
+      pointBorderColor: 'cyan',
+    },
+    {
+      label: 'Dataset 2',
+      data: [2, 29, 5, 5, 12, 13, 10, 15, 16, 17, 18, 19],
+      borderColor: 'orange',
+      backgroundColor: 'transparent',
+      pointBackgroundColor: 'orange',
+      pointBorderColor: 'orange',
+    },
+  ],
+};
 
 export default function Home() {
+  const [page, setPage] = useState(1);
+  const [mealTimeFilter, setMealTimeFilter] = useState<MealTime>('all');
+
+  const {
+    data: mealsData,
+    fetchNextPage,
+    hasNextPage,
+    refetch,
+    isFetchingNextPage,
+    isLoading,
+  } = useGetMeals(mealTimeFilter);
+
+  const [ref, { width }] = useMeasure<HTMLDivElement>();
+  const handleLoadMore = () => {
+    setPage(page + 1);
+    fetchNextPage({ pageParam: page + 1 });
+  };
+
+  const handleFilterClick = (mealType: MealTime) => {
+    setMealTimeFilter(mealType);
+  };
+
+  useEffect(() => {
+    refetch();
+  }, [mealTimeFilter, refetch]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <SimpleGrid h={['100%', '100%', 312]} columns={[1, 1, 2]}>
+        <ProgressSection />
+        <Box bg="dark.600">
+          <LineChart ref={ref} height={300} data={data} width={width} />
+        </Box>
+      </SimpleGrid>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Container mt={6} display="flex" flexDir="column" alignItems="center" maxW="container.lg">
+        <FilterList onFilter={handleFilterClick} />
+        {mealsData ? (
+          mealsData.pages.map((page, pageIndex) => <MealList key={page.data[pageIndex].id} items={page.data} />)
+        ) : (
+          <SimpleGrid mt={2} w="full" columns={[1, 2, 2, 4, 4]} gap={2} fontFamily="body">
+            {Array(8)
+              .fill(0)
+              .map(index => (
+                <Skeleton key={index} height="200px" />
+              ))}
+          </SimpleGrid>
+        )}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        {isFetchingNextPage && (
+          <SimpleGrid mt={2} w="full" columns={[1, 2, 2, 4, 4]} gap={2} fontFamily="body">
+            {Array(8)
+              .fill(0)
+              .map(index => (
+                <Skeleton key={index} height="200px" />
+              ))}
+          </SimpleGrid>
+        )}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        {hasNextPage && (
+          <Button w="296px" variant="unstyled" mt={7} onClick={handleLoadMore}>
+            <Center w="100%" h="100%" bgGradient="linear(to-l, primary.500, primary.300)" flexDirection="column">
+              記録をもっと見る
+            </Center>
+          </Button>
+        )}
+      </Container>
+    </>
+  );
 }
