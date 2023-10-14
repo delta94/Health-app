3567d32e1e6665ca861ff89e7c4012d94f612d5b
Jun Nguyen <jun.nguyen@ecoxplore.dev>
2023-10-14 21:09:32 +0700

    fix: fix minor bugs

--- components/organisms/DiaryList/index.tsx
+++ components/organisms/DiaryList/index.tsx
@@ -4,15 +4,10 @@ import { SimpleGrid, Text } from '@chakra-ui/react';
 
 export const DiaryList = ({ items, id }: { items: Diary[]; id: string }) => {
   return (
-    <>
-      <Text fontSize="22px" fontFamily="inter" fontWeight="400" w="full">
-        MY DIARY
-      </Text>
-      <SimpleGrid id={id} mt={2} columns={[1, 2, 2, 4, 4]} gap={3} fontFamily="body">
-        {items.map((diary, index) => (
-          <DiaryItem key={`${diary.id}-${index}`} {...diary}></DiaryItem>
-        ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid id={id} mt={2} columns={[1, 2, 2, 4, 4]} gap={3} fontFamily="body">
+      {items.map((diary, index) => (
+        <DiaryItem key={`${diary.id}-${index}`} {...diary}></DiaryItem>
+      ))}
+    </SimpleGrid>
   );
 };
