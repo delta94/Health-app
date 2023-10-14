22e24c0b9a65f7d32c918cabe263e183df9af875
Jun Nguyen <jun.nguyen@ecoxplore.dev>
2023-10-14 20:14:53 +0700

    fix: fix mealList

--- components/organisms/MealList/index.tsx
+++ components/organisms/MealList/index.tsx
@@ -1,12 +1,14 @@
 import { MealItem } from '@/components/molecules/MealItem';
 import { MealItemProps } from '@/types';
-import { SimpleGrid } from '@chakra-ui/react';
+import { GridItem, SimpleGrid } from '@chakra-ui/react';
 
 export const MealList = ({ items }: { items: MealItemProps[] }) => {
   return (
     <SimpleGrid mt={2} columns={[1, 2, 2, 4, 4]} gap={2} fontFamily="body">
       {items.map((meal, index) => (
-        <MealItem key={`${meal.id}-${index}-${meal.label}`} {...meal}></MealItem>
+        <GridItem key={`${meal.id}-${index}-${meal.label}`}>
+          <MealItem {...meal}></MealItem>
+        </GridItem>
       ))}
     </SimpleGrid>
   );
