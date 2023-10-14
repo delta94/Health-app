2f66a7eb3d2be8e331af405321a5e48e45fafced
Jun Nguyen <jun.nguyen@ecoxplore.dev>
2023-10-14 20:05:35 +0700

    refactor: use hooks mock service

--- app/registry.tsx
+++ app/registry.tsx
@@ -1,38 +1,19 @@
 'use client';
 
-import { setupClientMocks, setupServerMocks } from '@/__mocks__/setup';
 import MainLayout from '@/components/templates';
 import { setupReactQuery } from '@/config/react-query-config';
-import { isTrue } from '@/helpers';
+import { useMockService } from '@/hooks';
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { useState } from 'react';
 import { theme } from './theme';
 
-const mockServiceEnabled = isTrue(process.env.NEXT_PUBLIC_ENABLE_MOCK_SERVICE);
-let mockServiceStarted = false;
-
-if (mockServiceEnabled) {
-  setupServerMocks();
-}
-
-export default function RootStyleRegistry({ children }: PropsWithChildren<{}>) {
-  const [shouldRender, setShouldRender] = useState(!mockServiceEnabled);
+export default function RootStyleRegistry({ children }: React.PropsWithChildren<{}>) {
+  const shouldRender = useMockService();
   const [queryClient] = useState(setupReactQuery);
 
-  useEffect(() => {
-    if (mockServiceEnabled && !mockServiceStarted) {
-      mockServiceStarted = true;
-      setupClientMocks().then(_ => {
-        setShouldRender(true);
-      });
-    }
-  }, []);
-
-  if (!shouldRender) {
-    return null;
-  }
+  if (!shouldRender) return null;
 
   return (
     <QueryClientProvider client={queryClient}>
